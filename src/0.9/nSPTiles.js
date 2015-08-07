1b7034ab84cd4bc7ecbad3961bd3b90173b46286
IMTheNachoMan <imthenachoman@gmail.com>
2015-08-07 10:11:21 -0400

    Update and rename src/1.1/nSPTiles.js to src/0.9/nSPTiles.js

--- src/0.9/nSPTiles.js
+++ src/0.9/nSPTiles.js
@@ -2,7 +2,7 @@
  * ----------------------------------------------------------------------------
  * a JavaScript library for Window 8 style live tiles
  *
- * version : 1.1
+ * version : 0.9
  * author  : Anchal Nigam
  * e-mail  : imthenachoman@gmail.com
  * url     : https://github.com/imthenachoman/nSPTiles
